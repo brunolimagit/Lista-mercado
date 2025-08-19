8c427c03ba023703469b805a4458f783636f9dff
bruno lima <bru_jefferson@hotmail.com>
2025-08-19 00:10:06 -0300

    listaMercado.map, styly e centralização do componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import React from "react";
 import { ItemLista } from "./Components/ItemLista";
 
-function App () {
-  const listaMercado = [" Banana", "Maça", "Carne", 'Uva'];
+function App() {
+  const listaMercado = [" Banana", "Maça", "Carne", "Uva"];
   return (
-    <>
-      <h1>Lista de Compras</h1>
-      <input type="text" placeholder="Digite Um Item" className="flex" />
-      <button type="button" className="bg-[blue]">
-        Adicionar
-      </button>
+    <div className="w-full max-w-96">
+      <h1 className="text-3xl font-bold">Lista de Compras</h1>
+      <div className="">
+        <input type="text" placeholder="Digite Um Item" className="" />
+        <button type="button" className="">
+          Adicionar
+        </button>
+      </div>
 
       <ul>
-        {listaMercado.map((itemlista, index) =>(
+        {listaMercado.map((itemlista, index) => (
           <ItemLista itemlista={itemlista} key={index} />
         ))}
-      
       </ul>
-    </>
+    </div>
   );
-};
+}
 
 export default App;
