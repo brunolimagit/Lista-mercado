e1361ec7c75a04ca60bc105900c2a299b43052f8
bruno lima <bru_jefferson@hotmail.com>
2025-09-01 12:43:50 -0300

    style da lista de compras

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,16 @@ import { ItemLista } from "./Components/ItemLista";
 function App() {
   const listaMercado = [" Banana", "Maça", "Carne", "Uva"];
   return (
-    <div className="w-full max-w-96">
-      <h1 className="text-3xl font-bold">Lista de Compras</h1>
-      <div className="">
-        <input type="text" placeholder="Digite Um Item" className="" />
-        <button type="button" className="">
+    <div className="w-150 flex flex-col items-center mt- mx-auto p-4  rounded-lg   backdrop-blur-sm p-[40px]">
+      <h1 className="text-[4rem] font-bold text-[white]">Lista de compras</h1>
+      <div className="w-full flex gap-2 my-4 ">
+        <input type="text" placeholder="Digite Um Item" className=" rounded-[7px] p-3 w-full bg-white/70 font-bold text-[20px] text-gray-700 border-3 border-transparent focus:outline-none" />
+        <button type="button" className="bg-blue-500 text-white rounded-[7px] p-3 hover:bg-blue-700 cursor-pointer font-bold px-10">
           Adicionar
         </button>
       </div>
 
-      <ul>
+      <ul className="w-full flex flex-col justify-between gap-2">
         {listaMercado.map((itemlista, index) => (
           <ItemLista itemlista={itemlista} key={index} />
         ))}
--- src/Components/ItemLista.jsx
+++ src/Components/ItemLista.jsx
@@ -2,9 +2,9 @@ export function ItemLista({itemlista}) {
  
     
   return (
-    <li>
-      <p>{itemlista}</p>
-      <button className="bg-gray-300">Remover</button>
+    <li className="w-full flex justify-between bg-gray-200 p-3 rounded-[7px] items-center hover:scale-105 transition-all">
+      <p className="font-bold text-[20px] text-gray-700">{itemlista}</p>
+      <button className="bg-[red] font-bold text-white px-10 py-2 rounded-[7px] cursor-pointer hover:bg-red-900">Remover</button>
     </li>
   );
 }
