f978d6a71094c44e1a2cc1509210bf9112994a8c
bruno lima <bru_jefferson@hotmail.com>
2025-09-05 00:05:00 -0300

    adionando botão edit e funções no botão delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,60 @@
-import React from "react";
-import { ItemLista } from "./Components/ItemLista";
+import { useState, useRef } from "react";
+import { ItemLista,  } from "./Components/ItemLista";
 
 function App() {
-  const listaMercado = [" Banana", "Maça", "Carne", "Uva"];
-  return (
-    <div className="w-150 flex flex-col items-center mt- mx-auto p-4  rounded-lg   backdrop-blur-sm p-[40px]">
-      <h1 className="text-[4rem] font-bold text-[white]">Lista de compras</h1>
-      <div className="w-full flex gap-2 my-4 ">
-        <input type="text" placeholder="Digite Um Item" className=" rounded-[7px] p-3 w-full bg-white/70 font-bold text-[20px] text-gray-700 border-3 border-transparent focus:outline-none" />
-        <button type="button" className="bg-blue-500 text-white rounded-[7px] p-3 hover:bg-blue-700 cursor-pointer font-bold px-10">
-          Adicionar
-        </button>
-      </div>
+  const [listaMercado, setListaMercado] = useState([]);
+  const inputAdd = useRef(null)
 
-      <ul className="w-full flex flex-col justify-between gap-2">
-        {listaMercado.map((itemlista, index) => (
-          <ItemLista itemlista={itemlista} key={index} />
-        ))}
-      </ul>
-    </div>
+  const adicionarItem = () => {
+    const novaLista = [...listaMercado];
+    const valorInput = inputAdd.current.value;
+
+    if (valorInput) {
+      novaLista.push(valorInput);
+  
+      inputAdd.current.value = "";
+  
+      setListaMercado(novaLista);
+
+    }
+
+  };
+
+  return (
+    <>
+      <div className="flex w-[500px] flex-col flex-wrap items-center rounded-lg p-10 backdrop-blur-sm my-[5%] mx-auto">
+        <h1 className="text-[3rem] font-bold text-white">Lista de compras</h1>
+        <div className="my-4 flex w-full gap-2 bg-transparent">
+          <input
+            type="text"
+            placeholder="Digite um item"
+            className="w-full rounded-[7px] border-3 border-transparent bg-white/50 p-2 text-[16px] font-bold text-gray-700 focus:outline-none"
+            ref={inputAdd}
+          />
+          <button
+            type="button"
+            onClick={adicionarItem}
+            className="cursor-pointer rounded-[7px] bg-blue-500 p-2 px-7 font-bold text-white hover:bg-blue-700"
+          >
+            Adicionar
+          </button>
+        </div>
+
+      <div className="flex w-full text-center justify-center"> 
+        {listaMercado.length > 0 ? (
+          <ul className="flex w-full flex-col gap-2 pb-2">
+            {listaMercado.map((itemlista, index) => (
+              <ItemLista itemlista={itemlista} key={index} listaMercado={listaMercado} setListaMercado={setListaMercado} />
+            ))}
+          </ul>
+        ) : (
+          <p className=" justify-center text-center font-bold text-white">
+            Você não tem nenhum item na lista
+          </p>
+        )}
+      </div>
+      </div>
+    </>
   );
 }
-
 export default App;
--- src/Components/ItemLista.jsx
+++ src/Components/ItemLista.jsx
@@ -1,10 +1,23 @@
-export function ItemLista({itemlista}) {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faCartShopping, faPlus, faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+
+export function ItemLista({itemlista, listaMercado , setListaMercado}) {
  
-    
+    const removerItem = () => {
+      const novaLista = [...listaMercado]
+      const novaListaFiltrada = novaLista.filter((item) => item !== itemlista);
+      setListaMercado(novaListaFiltrada)
+    }
   return (
-    <li className="w-full flex justify-between bg-gray-200 p-3 rounded-[7px] items-center hover:scale-105 transition-all">
-      <p className="font-bold text-[20px] text-gray-700">{itemlista}</p>
-      <button className="bg-[red] font-bold text-white px-10 py-2 rounded-[7px] cursor-pointer hover:bg-red-900">Remover</button>
+    <li className="w-full flex justify-between bg-gray-200 p-1 rounded-[7px] items-center hover:scale-105 transition-all">
+      <p className="font-bold text-[16px] text-gray-700 ml-[10px]">{itemlista}</p>
+      <div className="flex gap-2 mr-[10px]">
+      <button className="bg-[green] font-bold text-white px-7 py-2 rounded-[7px] cursor-pointer hover:bg-green-900" >
+        <FontAwesomeIcon icon={faPenToSquare} />
+</button>
+      <button className="bg-[red] font-bold text-white px-7 py-2 rounded-[7px] cursor-pointer hover:bg-red-900 text-[17px]" onClick={removerItem}> <FontAwesomeIcon icon={faTrash} /></button>
+      </div>
     </li>
   );
 }
+
